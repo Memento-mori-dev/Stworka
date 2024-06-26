649517c825d98b3fb24922b6f4c4f77425cda83d
Kirill Pet <job.mori.1967@gmail.com>
2024-06-26 23:27:53 +0300

    сделал анимацию по процентам + убрал лишний код

--- script.js
+++ script.js
@@ -5,19 +5,9 @@ const button = document.querySelector('.button');
 button.addEventListener('click', (e) => {
     button.classList.toggle('active');
 
-    let content = e.target.closest('.checkList').querySelector('.checkList-content');
-    let list = content.querySelector('ul');
-    let listCheckbox = list.querySelectorAll('input');
-
-    console.log(listCheckbox);
-
     if (button.classList.contains('active')) {
         button.setAttribute('aria-expanded', 'true');
-        list.setAttribute('aria-hidden', 'false');
-        listCheckbox.forEach(link => link.setAttribute('tabindex', '0'));
     } else {
         button.setAttribute('aria-expanded', 'false');
-        list.setAttribute('aria-hidden', 'true');
-        listCheckbox.forEach(link => link.setAttribute('tabindex', '-1'));
     }
 })
